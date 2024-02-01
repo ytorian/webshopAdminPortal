d831c69bc26b34c84d271ae488440dc337a62fce
ytorian <sam111925@zcll.nl>
2024-02-01 14:12:33 +0100

    initial upload

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,6 @@
 
 <x-layouts.authenticated-layout>
     <h1 style="font-size: xx-large; color: white; text-align: center; padding-top: 50px">
-        Welcome to the home page
+        Welcome to the home page!
     </h1>
 </x-layouts.authenticated-layout>

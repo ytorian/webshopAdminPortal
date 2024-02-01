9d046d673a75899398512b544ac09f4a6466209a
ytorian <sam111925@zcll.nl>
2024-02-01 14:10:39 +0100

    deleted

--- app/Helpers/visualizeQuery.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function visualizeQuery ($form, $value1, $value2 = null, $value3 = null, $category = null)
-{
-    if ($form == 'category') {
-        return "INSERT INTO categories (name) <br> VALUES (" . $value1 . ")";
-    } else
-
-}
